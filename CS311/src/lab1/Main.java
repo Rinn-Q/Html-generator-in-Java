068120d4a4b472bc3544dba8bb7465617fc7ff21
Natsu0425 <152431605+Natsu0425@users.noreply.github.com>
2024-05-09 19:16:59 +0800

    Delete CS311 directory

--- CS311/src/lab1/Main.java
+++
@@ -1,8 +0,0 @@
-package lab1;
-
-public class Main {
-    public static void main(String[] args) {
-        User user = new User();
-        user.using();
-    }
-}
--- CS311/src/lab1/Print.java
+++
@@ -1,6 +0,0 @@
-package lab1;
-
-public interface Print {
-    void printMenu();
-    void printEdit();
-}
